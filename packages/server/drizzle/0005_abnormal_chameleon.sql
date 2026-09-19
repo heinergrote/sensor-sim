697c7a0baaaa2fb9f34e35b16388961a2ff5e438
Heiner Grote <mail@h9e.de>
2026-09-19 18:13:34 +0200

    fix migration

--- packages/server/drizzle/0005_abnormal_chameleon.sql
+++ packages/server/drizzle/0005_abnormal_chameleon.sql
@@ -1,3 +1,11 @@
-ALTER TABLE "sim_configs" ALTER COLUMN "share_token" SET DATA TYPE text;--> statement-breakpoint
-ALTER TABLE "sim_configs" ALTER COLUMN "share_token" SET DEFAULT '';--> statement-breakpoint
-ALTER TABLE "sim_configs" ALTER COLUMN "share_token" SET NOT NULL;
+ALTER TABLE "sim_configs"
+    ALTER COLUMN "share_token" SET DATA TYPE text;--> statement-breakpoint
+
+ALTER TABLE "sim_configs"
+    ALTER COLUMN "share_token" SET DEFAULT '';--> statement-breakpoint
+
+UPDATE "sim_configs"
+SET "share_token" = '';--> statement-breakpoint
+
+ALTER TABLE "sim_configs"
+    ALTER COLUMN "share_token" SET NOT NULL;
